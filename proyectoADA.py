6909354cdc4d58a62fee73e38cbc42cf18eb0635
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-05-10 19:37:29 -0600

    Another way to try

--- proyectoADA.py
+++ proyectoADA.py
@@ -1,11 +1,6 @@
 import networkx as nx
-
-x1=True
-x2=False
-x3=False
-x4=True
-
-import networkx as nx
+import pulp
+import matplotlib.pyplot as plt
 
 # Valores de las variables
 values = {1: True, 2: False, 3: False, 4: True}
@@ -17,67 +12,61 @@ formula = [
     [(1, False), (3, False), (4, True)]
 ]
 
-# Verificar si los valores satisfacen la fórmula
-satisfiable = all(any(values[var] == sign for var, sign in clause) for clause in formula)
+# Crear un grafo vacío
+G = nx.Graph()
 
-if not satisfiable:
-    print("Los valores proporcionados no satisfacen la fórmula.")
-else:
+# Añadir vértices al grafo
+for clause in formula:
+    for literal in clause:
+        var = literal[0]
+        G.add_node(f'x{var}')
+        G.add_node(f'¬x{var}')
 
-    # Crear un grafo vacío
-    G = nx.Graph()
+# Añadir aristas entre las variables y sus negaciones
+for i in range(1, 5):
+    G.add_edge(f'x{i}', f'¬x{i}')
 
-    # Añadir vértices al grafo
-    for clause in formula:
-        for literal in clause:
-            var = literal[0]
-            G.add_node(f'x{var}')
-            G.add_node(f'¬x{var}')
+# Añadir aristas entre literales de diferentes cláusulas
+for i in range(len(formula)):
+    for j in range(i + 1, len(formula)):
+        for literal1 in formula[i]:
+            for literal2 in formula[j]:
+                if literal1[0] != literal2[0]:
+                    G.add_edge(str(literal1), str(literal2))
 
-    # Añadir aristas entre las variables y sus negaciones
-    for i in range(1, 5):
-        G.add_edge(f'x{i}', f'¬x{i}')
+# Añadir aristas correspondientes a la cláusula solo si el literal no está satisfecho
+for clause in formula:
+    unsatisfied_literals = [str(literal) for literal in clause if values[literal[0]] != literal[1]]
+    for i, u in enumerate(unsatisfied_literals):
+        for v in unsatisfied_literals[i + 1:]:
+            G.add_edge(u, v)
 
-    # Añadir aristas entre literales de diferentes cláusulas
-    for i in range(len(formula)):
-        for j in range(i + 1, len(formula)):
-            for literal1 in formula[i]:
-                for literal2 in formula[j]:
-                    if literal1[0] != literal2[0]:
-                        G.add_edge(str(literal1), str(literal2))
+# Crear el problema de programación lineal entera
+vertex_cover_problem = pulp.LpProblem("VertexCover", pulp.LpMinimize)
 
-    # Añadir aristas correspondientes a la cláusula solo si el literal no está satisfecho
-    for clause in formula:
-        unsatisfied_literals = [str(literal) for literal in clause if values[literal[0]] != literal[1]]
-        for i, u in enumerate(unsatisfied_literals):
-            for v in unsatisfied_literals[i + 1:]:
-                G.add_edge(u, v)
+# Crear variables binarias para cada nodo
+node_vars = {node: pulp.LpVariable(f"{node}", 0, 1, pulp.LpInteger) for node in G.nodes()}
 
-    # Usar el algoritmo de aproximación para Vertex Cover
-    vertex_cover = nx.algorithms.approximation.vertex_cover.min_weighted_vertex_cover(G)
+# Agregar la función objetivo: minimizar la suma de las variables binarias
+vertex_cover_problem += pulp.lpSum(node_vars)
 
-    # Visualizar el grafo y resaltar el Vertex Cover
-    import matplotlib.pyplot as plt
+# Agregar restricciones: para cada arista, al menos uno de sus nodos debe estar en el Vertex Cover
+for u, v in G.edges():
+    vertex_cover_problem += node_vars[u] + node_vars[v] >= 1
 
-    # Crear dos listas de nodos, una para variables y otra para sus negaciones
-    var_nodes = [f'x{i}' for i in range(1, 5)]
-    neg_nodes = [f'¬x{i}' for i in range(1, 5)]
+# Resolver el problema de programación lineal entera
+vertex_cover_problem.solve()
 
-    # Visualizar el grafo y resaltar el Vertex Cover
-    pos = nx.circular_layout(G)
+# Obtener el Vertex Cover mínimo
+min_vertex_cover = [node for node, node_var in node_vars.items() if node_var.value() == 1]
 
-    # Dibujar los nodos con el color y tamaño especificados
-    nx.draw(G, pos, with_labels=False, node_color='lightblue', node_size=500)
+print(f"El número de vértices en el Vertex Cover mínimo es: {len(min_vertex_cover)}")
+print(f"Los vértices en el Vertex Cover mínimo son: {min_vertex_cover}")
 
-    # Dibujar los nodos del Vertex Cover con el color y tamaño especificados
-    nx.draw_networkx_nodes(G, pos, nodelist=vertex_cover, node_color='red', node_size=500)
-
-    # Dibujar las etiquetas de los nodos con un tamaño de fuente más pequeño
-    labels = {node: node for node in G.nodes()}
-    nx.draw_networkx_labels(G, pos, labels, font_size=8, font_weight='bold')
-
-    plt.show()
-
-
-    # Mostrar el número de vértices en el Vertex Cover
-    print(f"El número de vértices en el Vertex Cover es: {len(vertex_cover)}")
+# Visualizar el grafo y resaltar el Vertex Cover mínimo
+pos = nx.circular_layout(G)
+nx.draw(G, pos, with_labels=False, node_color='lightblue', node_size=500)
+nx.draw_networkx_nodes(G, pos, nodelist=min_vertex_cover, node_color='red', node_size=500)
+labels = {node: node for node in G.nodes()}
+nx.draw_networkx_labels(G, pos, labels, font_size=8, font_weight='bold')
+plt.show()
